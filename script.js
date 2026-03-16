e361bd0b12bdd21726ccab7bf4d35efc83891ba2
yukihopkins76-gif <yukihopkins76@gmail.com>
2026-03-16 17:51:21 +0530

    Add event listener to test button

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('testBtn').addEventListener('click', function() {
+    alert('Yuki Bhai, 28,000 line ka rasta saaf hai!');
+    console.log('Ghost Builder Engine: Online');
+});
